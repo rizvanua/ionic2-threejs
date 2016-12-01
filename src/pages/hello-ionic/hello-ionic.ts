ad6f54361351d903119e5b6ad395db0cd6308cf3
rizvanua <rizvanua@gmail.com>
2016-12-01 06:23:45 +0200

    change

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -2,7 +2,6 @@ import {Component} from '@angular/core';
 import {RegistrationPage} from "../registration/registration";
 import {NavController} from "ionic-angular";
 import {SiginComponent} from "../sigin/sigin";
-import * as YouTubePlayer from 'youtube-player';
 
 
 @Component({
@@ -16,22 +15,17 @@ export class HelloIonicPage {
   }
   ionViewDidLoad() {
     console.log('Hello NewentryPage Page');
-    this.player = YouTubePlayer('video-player');
-    this.player.loadVideoById('M7lc1UVf-VE');
-    this.player.playVideo();
 
   }
   ngAfterContentInit() {
 
   }
   openSiginPage() {
-    this.player.stopVideo();
-    // navigate to the new page if it is not the current page
+       // navigate to the new page if it is not the current page
     this.navCtrl.push(SiginComponent);
 
   }
   openRegistrationPage() {
-    this.player.stopVideo();
     // navigate to the new page if it is not the current page
     this.navCtrl.push(RegistrationPage);
 
